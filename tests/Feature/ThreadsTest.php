c5347cadb5f358ffb65ded9dfb1ed2d214ead428
Eva <evangeline013@gmail.com>
2018-03-17 09:49:50 -0400

    Finish adding tests for threads

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -9,12 +9,18 @@ class ThreadsTest extends TestCase
 {
     /** @test */
     use RefreshDatabase;
-    public function a_user_can_browse_threads()
+    public function a_user_can_view_all_threads()
     {
         $thread = factory('App\Thread')->create();
         $response = $this->get('/threads');
         $response->assertSee($thread->title);
 
+    }
+
+    /** @test */
+    public function a_user_can_view_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
         $response = $this->get('/threads/'.$thread->id);
         $response->assertSee($thread->title);
     }
