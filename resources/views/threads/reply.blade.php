ee177bf83b802e44e8016678f492860bab9bc9ad
Eva <evangeline013@gmail.com>
2018-03-29 16:50:57 -0400

    Reduce queries using eager load

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,5 +1,23 @@
 <div class="card-header">
-    {{$reply->owner->name}} said {{$reply->created_at->diffForHumans()}}
+    <div class="level">
+
+        <h5 class="flex">
+            <a href="#">
+                {{$reply->owner->name}} said {{$reply->created_at->diffForHumans()}}...
+            </a>
+        </h5>
+
+        <div>
+            <form method="POST" action="/replies/{{$reply->id}}/favorites">
+                {{csrf_field()}}
+
+                <button type="submit" class="btn btn-default" {{$reply->isFavorited()? 'disabled' : ''}}>
+                    {{$reply->favorites_count}} {{str_plural('Favorite', $reply->favorites_count)}}
+                </button>
+            </form>
+        </div>
+
+    </div>
 </div>
 
 <div class="card-body">
